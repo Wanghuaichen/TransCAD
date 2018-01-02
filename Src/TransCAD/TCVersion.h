399f8a4005a0df5db80533a79d25d6cd3a6d5b93
Youngki <kyk5415@gmail.com>
2018-01-03 08:04:48 +0900

    TransCAD VERSION 6.1.2 정보 수정

--- Src/TransCAD/TCVersion.h
+++ Src/TransCAD/TCVersion.h
@@ -1,7 +1,7 @@
 // Version number
 #define TCMajor		"6"
 #define TCMinor		"1"
-#define TCRevision	"1"
+#define TCRevision	"2"
 
-#define TCReleaseDate "2017/09/06"
-#define TCRepositoryURL ""
+#define TCReleaseDate "2018/01/03"
+#define TCRepositoryURL "https://github.com/Macro-parametrics/TransCAD"
